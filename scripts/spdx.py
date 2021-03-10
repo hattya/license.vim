0c7961cd9cf10040777d5bffa1777109aa9dc645
Akinori Hattori <hattya@gmail.com>
2021-03-10 21:08:18 +0900

    Update to SPDX License List 3.12

--- scripts/spdx.py
+++ scripts/spdx.py
@@ -1,8 +1,8 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 #
 # spdx
 #
-#   Copyright (c) 2019 Akinori Hattori <hattya@gmail.com>
+#   Copyright (c) 2019-2021 Akinori Hattori <hattya@gmail.com>
 #
 #   SPDX-License-Identifier: MIT
 #
@@ -10,41 +10,48 @@
 import json
 import operator
 import os
-try:
-    from urllib.request import urlopen
-except ImportError:
-    from urllib2 import urlopen
+from typing import Dict, Iterator
+import urllib.request
 
 
-def main():
+def main() -> None:
     datadir = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'license')
     if not os.path.exists(datadir):
         os.makedirs(datadir)
     # licenses
     with open(os.path.join(datadir, 'SPDX.licenses'), 'w') as fp:
-        for v in load('licenses.json', 'licenseId'):
+        fsf_libre = set(load_fsf())
+        for v in load_spdx('licenses.json', 'licenseId'):
             fp.write(v['licenseId'])
             if v.get('isDeprecatedLicenseId'):
                 fp.write('\tdeprecated')
-            if v.get('isFsfLibre'):
+            if v['licenseId'] in fsf_libre:
                 fp.write('\tfsf')
             if v.get('isOsiApproved'):
                 fp.write('\tosi')
             fp.write('\n')
     # exceptions
     with open(os.path.join(datadir, 'SPDX.exceptions'), 'w') as fp:
-        for v in load('exceptions.json', 'licenseExceptionId'):
+        for v in load_spdx('exceptions.json', 'licenseExceptionId'):
             fp.write(v['licenseExceptionId'])
             if v.get('isDeprecatedLicenseId'):
                 fp.write('\tdeprecated')
             fp.write('\n')
 
 
-def load(name, key):
-    resp = urlopen('https://spdx.org/licenses/{}'.format(name))
-    if resp.getcode() == 200:
-        for v in sorted(json.load(resp)[os.path.splitext(name)[0]], key=operator.itemgetter(key)):
-            yield v
+def load_spdx(name: str, key: str) -> Iterator[Dict[str, str]]:
+    with urllib.request.urlopen('https://spdx.org/licenses/{}'.format(name)) as resp:
+        if resp.getcode() == 200:
+            yield from sorted(json.load(resp)[os.path.splitext(name)[0]], key=operator.itemgetter(key))
+
+
+def load_fsf() -> Iterator[str]:
+    with urllib.request.urlopen('https://wking.github.io/fsf-api/licenses-full.json') as resp:
+        if resp.getcode() == 200:
+            for k, v in json.load(resp)['licenses'].items():
+                if 'libre' in v['tags']:
+                    yield k
+                    yield from v.get('identifiers', {}).get('spdx', [])
 
 
 if __name__ == '__main__':
